5052e7090d9d4440886dd262aff5491bc5107d87
Yehonal <yehonal.azeroth@gmail.com>
2018-12-23 11:23:16 +0100

    non blocking functions

--- assets/js/red-links.js
+++ assets/js/red-links.js
@@ -1,36 +1,38 @@
-
 (function ($) {
     //
     // RED LINK FEATURE (Hacky)
     // TODO: filter external links
     $.fn.redLinks = function () {
-        $('a').each(function () {
-            // avoid red link for external urls
-            if (this.hostname != window.location.hostname)
-                return;
+        setTimeout(
+            function () {
+                $('a').each(function () {
+                    // avoid red link for external urls
+                    if (this.hostname != window.location.hostname)
+                        return;
 
-            var ext = this.href.split('.').pop().split(/\#|\?/)[0];
+                    var ext = this.href.split('.').pop().split(/\#|\?/)[0];
 
-            // [Performance tip] pessimistic condition based on the fact that
-            // markdown files are automatically converted in html
-            // if they are part of the wiki (the real check is right below)
-            if (ext.toLowerCase() == "md" || ext.toLowerCase() == "markdown")
-                $(this).css('color', 'red');
+                    // [Performance tip] pessimistic condition based on the fact that
+                    // markdown files are automatically converted in html
+                    // if they are part of the wiki (the real check is right below)
+                    if (ext.toLowerCase() == "md" || ext.toLowerCase() == "markdown")
+                        $(this).css('color', 'red');
 
-            var that = this;
-            $.ajax({
-                type: 'HEAD',
-                url: this.href,
-                success: function () {
-                    $(that).css('color', '');
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    if (xhr.status == 404) {
-                        $(that).css('color', 'red');
-                    }
-                }
-            });
-        });
+                    var that = this;
+                    $.ajax({
+                        type: 'HEAD',
+                        url: this.href,
+                        success: function () {
+                            $(that).css('color', '');
+                        },
+                        error: function (xhr, ajaxOptions, thrownError) {
+                            if (xhr.status == 404) {
+                                $(that).css('color', 'red');
+                            }
+                        }
+                    });
+                });
+            }, 0);
     };
 
 })(jQuery);
